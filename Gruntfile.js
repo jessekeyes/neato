77f8ae8f3220f1dbadad32f6fec3dda05980c1de
Jesse Keyes <jessekeyes@gmail.com>
2014-02-03 13:36:54 -0800

    initial framework set up with some mobile responsives already set

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     watch: {
       css: {
-        files: '<%= pkg.src %>scss/*.scss',
+        files: '<%= pkg.src %>/scss/*.scss',
         tasks: 'sass:build'
       }
     },
