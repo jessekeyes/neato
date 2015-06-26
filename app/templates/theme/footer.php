803224152c2d0e9c0567d2186d984310e5393367
Jesse Keyes <jessekeyes@gmail.com>
2015-06-26 16:23:36 -0700

    footer nav now pulls footer nav widget, not primary

--- app/templates/theme/footer.php
+++ app/templates/theme/footer.php
@@ -11,16 +11,17 @@
 </div> <!-- /.container -->
 
 <footer class="site-footer">
+
+    <nav class="nav-footer">
+      <?php wp_nav_menu( array( 'theme_location' => 'footer_nav', 'container' => '') ); ?>
+    </nav>
+    
     <p>
       <?php bloginfo('name'); ?> is proudly powered by
       <a href="http://wordpress.org/">WordPress</a>.
       <br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>.
       <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
     </p>
-
-    <nav class="nav-footer">
-      <?php wp_nav_menu( array( 'theme_location' => 'primary_nav', 'container' => '') ); ?>
-    </nav>
 </footer>
 
 			   
