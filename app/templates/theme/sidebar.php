d479a6b64091a5bdd130f7c0b50288581b6a1933
Jesse Keyes <jessekeyes@gmail.com>
2015-06-26 16:01:59 -0700

    added else conditional to sidebar widget display, fixed list formatting by adding parent <ul>

--- app/templates/theme/sidebar.php
+++ app/templates/theme/sidebar.php
@@ -7,16 +7,20 @@
  */
 ?>
 <aside class="sidebar">
+
   <?php if ( is_active_sidebar( 'sidebar-1' ) ) :
     dynamic_sidebar( 'sidebar-1' );
-  endif; ?>
+  else: ?>
   <section>
     <?php get_search_form(); ?>
   </section>
   
   <nav role="navigation">
-    <?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
-
+  	<ul>
+	    <?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
+	  </ul>
   </nav>
+	<?php endif; ?>
+
 </aside>
 
